ada2b28157ebe2a7a116f69d9efe79f33250fbc6
Jean Prunneaux <jean@prunneaux.com>
2017-08-09 22:47:21 +0200

    Prepare next release

--- time2backup.sh
+++ time2backup.sh
@@ -9,7 +9,7 @@
 #  MIT License                                         #
 #  Copyright (c) 2017 Jean Prunneaux                   #
 #                                                      #
-#  Version 1.0.0 (2017-08-09)                          #
+#  Version 1.1.0-beta.1 (2017-07-27)                   #
 #                                                      #
 ########################################################
 
@@ -18,7 +18,7 @@
 #  VARIABLES DECLARATION  #
 ###########################
 
-version="1.0.0"
+version="1.1.0-beta.1"
 
 portable_mode=false
 user=""
