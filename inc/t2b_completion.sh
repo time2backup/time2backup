93341c360b31a2eae0970d3a1115c8c6045a3310
Jérôme Pasquier <rainbru42@gmail.com>
2017-11-11 20:40:33 +0100

    Add bash completion script working on time2backup/time2backup#124

---
+++ inc/t2b_completion.sh
@@ -0,0 +1,37 @@
+#
+# time2backup bash auto-completion handling
+#
+# This file is part of time2backup (https://time2backup.github.io)
+#
+# MIT License
+# Copyright (c) 2017 Jean Prunneaux
+#
+
+_t2b_complete()
+{
+    local cur_word prev_word type_list
+
+    # COMP_WORDS is an array of words in the current command line.
+    # COMP_CWORD is the index of the current word (the one the cursor is
+    # in). So COMP_WORDS[COMP_CWORD] is the current word; we also record
+    # the previous word here, although this specific script doesn't
+    # use it yet.
+    cur_word="${COMP_WORDS[COMP_CWORD]}"
+    prev_word="${COMP_WORDS[COMP_CWORD-1]}"
+
+    # Only perform completion if the current word starts with a dash ('-'),
+    # meaning that the user is trying to complete an option.
+    if [[ ${cur_word} == -* ]] ; then
+        # COMPREPLY is the array of possible completions, generated with
+        # the compgen builtin.
+        COMPREPLY=($(compgen -W '--console --log-level --verbose-level --destination --config --debug --version --help' -- ${cur_word}))
+	
+    else
+        COMPREPLY=( $(compgen -W 'backup restore history explore status stop clean config install uninstall' -- ${cur_word}))
+    fi
+    return 0
+}
+
+# Register _pss_complete to provide completion for the following commands
+complete -F _t2b_complete time2backup
+# To remove `complete -W "" time2backup`
